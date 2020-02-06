e91d50e3759044920b3f099bc490f01fdb32aaf0
edwin <cubillos.edwin.ext@nokia.com>
2020-02-06 04:53:07 -0500

    devel start app

---
+++ frontend/src/components/LineGraphs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LineGraph = () => {
+  return (
+    <div className="LineGraph">
+        <input type="text" name="csv_data" size="40"/><br/>
+        <button type="submit">process</button>
+    </div>      
+  )  
+};
+
+export default LineGraph;
