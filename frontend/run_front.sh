5cdac2e4be01cda6e1416f4942178b8000b14ed6
Edwin Cubillos <edwin.cubillos.ext@nokia.com>
2021-07-20 17:33:12 -0500

    devel :sunny: :fire: update lib and webpack

--- frontend/run_front.sh
+++ frontend/run_front.sh
@@ -4,5 +4,6 @@ echo "** Installing packages ** "
 npm install
 echo "** Starting dev server**"
 npm run start
+#npm run start-react PORT=8080
 
 echo "** Dev server ok  running on localhost:8080 **"
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -52,6 +52,8 @@ module.exports = {
     },
     devServer: {
         historyApiFallback: true,
+        host: '0.0.0.0',
+        port: 8080,
     },
     plugins: [
         new HtmlWebPackPlugin({
